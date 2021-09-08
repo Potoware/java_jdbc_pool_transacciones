884ba1c344a6e9632a2d5b1c05198729e5cd3993
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-08 18:12:24 -0500

    CRUD JDBC POOL con transacciones Parte3

--- src/main/java/com/potoware/java/jdbc/repositorio/CategoriaRepositorio.java
+++ src/main/java/com/potoware/java/jdbc/repositorio/CategoriaRepositorio.java
@@ -1,6 +1,7 @@
 package com.potoware.java.jdbc.repositorio;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,8 +13,7 @@ import org.apache.commons.dbcp2.DelegatingPreparedStatement;
 
 import com.potoware.java.jdbc.models.Categoria;
 
-public class CategoriaRepositorio implements Repositorio{
-	
+public class CategoriaRepositorio implements Repositorio<Categoria> {
 
 	private Connection conn;
 
@@ -21,29 +21,25 @@ public class CategoriaRepositorio implements Repositorio{
 		this.conn = conn;
 	}
 
-
 	@Override
 	public List<Categoria> listar() throws SQLException {
 		List<Categoria> categorias = new ArrayList<>();
-		try(Statement stmt = conn.createStatement();
-				ResultSet resultSet = stmt.executeQuery("SELECT * FROM categorias")){
-			while(resultSet.next()) {
+		try (Statement stmt = conn.createStatement();
+				ResultSet resultSet = stmt.executeQuery("SELECT * FROM categorias")) {
+			while (resultSet.next()) {
 				categorias.add(crearCategoria(resultSet));
 			}
 		}
 		return categorias;
 	}
 
-
-	
-
 	@Override
 	public Categoria porId(Long id) throws SQLException {
 		Categoria categoria = null;
-		try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM categorias as c WHERE c.id=?")){
+		try (PreparedStatement stmt = conn.prepareStatement("SELECT * FROM categorias as c WHERE c.id=?")) {
 			stmt.setLong(1, id);
-			try(ResultSet resultSet = stmt.executeQuery()){
-				if(resultSet.next()) {
+			try (ResultSet resultSet = stmt.executeQuery()) {
+				if (resultSet.next()) {
 					categoria = crearCategoria(resultSet);
 				}
 			}
@@ -52,19 +48,50 @@ public class CategoriaRepositorio implements Repositorio{
 	}
 
 	@Override
-	public Object guardar(Object t) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Categoria guardar(Categoria t) throws SQLException {
+
+		String sql;
+		if (t.getId() != null && t.getId() > 0) {
+			sql = "UPDATE categorias SET nombre=? where id=?";
+		} else {
+			sql = "INSERT INTO categorias (nombre) VALUES(?)";
+		}
+		try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+			stmt.setString(1, t.getNombre());
+
+			if (t.getId() != null && t.getId() > 0) {
+				stmt.setLong(2, t.getId());
+			}
+
+			stmt.executeUpdate();
+
+			//
+			if (t.getId() != null) {
+				try (ResultSet resultSet = stmt.getGeneratedKeys()) {
+					if (resultSet.next()) {
+						t.setId(resultSet.getLong(1));
+					}
+				}
+			}
+
+		}
+
+		return t;
+
 	}
 
 	@Override
 	public void eliminar(Long id) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		try(PreparedStatement stmt = conn.prepareStatement("DELETE FROM categorias WHERE id=?")){
+			stmt.setLong(1, id);
+			stmt.executeUpdate();
+		}
+
 	}
-	
+
 	private Categoria crearCategoria(ResultSet resultSet) throws SQLException {
-		Categoria categoria =new Categoria();
+		Categoria categoria = new Categoria();
 		categoria.setId(resultSet.getLong("id"));
 		categoria.setNombre(resultSet.getString("nombre"));
 		return categoria;
